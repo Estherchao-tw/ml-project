20e59d930da2e7161bdd18591dde0bf5417213a3
Estherchao-tw <xperia.chao@gmail.com>
2023-08-28 22:55:31 +0800

    tensorflow fixed

--- tensorflow/tensor.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-print('GPU', tf.config.list_physical_devices('GPU'))
-
-a = tf.constant(2.0)
-b = tf.constant(4.0)
-print(a+b)
